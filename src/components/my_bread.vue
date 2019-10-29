ddbe14740e2f39b91292c3382d417366e676d87d
**** <1069319584@qq.com>
2019-10-29 19:40:57 +0800

    内容管理

--- src/components/my_bread.vue
+++ src/components/my_bread.vue
@@ -1,7 +1,7 @@
 <template>
   <el-breadcrumb separator-class="el-icon-arrow-right">
     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+    <el-breadcrumb-item><slot></slot></el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
