152b30db928050044ad59120e7bdf65702d7016b
**** <1069319584@qq.com>
2019-11-04 20:20:24 +0800

    评论管理完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -4,16 +4,96 @@
       <div slot="header">
         <my-bread>评论管理</my-bread>
       </div>
+      <!-- 表格 -->
+      <el-table :data="articles">
+        <el-table-column label="标题" width="400" prop="title"></el-table-column>
+        <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+        <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">{{scope.row.comment_status?'正常':'关闭'}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="scope">
+            <el-button
+              @click="toggleStatus(scope.row.id,false)"
+              v-if="scope.row.comment_status"
+              type="danger"
+              size="small"
+            >关闭评论</el-button>
+            <el-button
+              @click="toggleStatus(scope.row.id,true)"
+              v-else
+              type="success"
+              size="small"
+            >打开评论</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        style="margin-top:20px"
+        background
+        layout="prev, pager, next"
+        :total="total"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="pager"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      articles: [],
+      // 总条数
+      total: 0,
+      reqParams: {
+        per_page: 20,
+        page: 1,
+        response_type: 'comment'
+      }
+    }
+  },
+  created () {
+    this.getArticles()
+  },
+  methods: {
+    async getArticles () {
+      const {
+        data: { data }
+      } = await this.$axios.get('articles', { params: this.reqParams })
+      // 渲染
+      this.articles = data.results
+      // 总条数
+      this.total = data.total_count
+    },
+    // 分页
+    pager (newPage) {
+      this.reqParams.page = newPage
+      this.getArticles()
+    },
+    // 切换 打开 与 关闭
+    toggleStatus (id, status) {
+      const text = status ? '您确认要打开评论吗？' : '您确认关闭评论吗，如果关闭用户将无法对该文章进行评论。'
+      this.$confirm(text, '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        // 确认时候
+        const { data: { data } } = await this.$axios.put(`comments/status?article_id=${id}`, { allow_comment: status })
+        // 提示
+        this.$message.success(data.allow_comment ? '打开评论成功' : '关闭评论成功')
+        // 更新当前列表
+        this.getArticles()
+      }).catch(() => {})
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang='less'>
 </style>
